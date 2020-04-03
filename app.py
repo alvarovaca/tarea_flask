9e71688487c6ae860156071c03f19cd0732b2b71
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-04-03 08:58:12 +0200

    Página inicio terminada.

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask, render_template, abort
+from lxml import etree
+app = Flask (__name__)
+
+@app.route('/')
+def inicio():
+    return render_template("inicio.html")
+
+app.run(debug=True)
