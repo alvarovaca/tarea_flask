44206f6ae409b14982858d2e7558898aea338d6b
Álvaro Vaca Ferreras <avacaferreras@gmail.com>
2020-04-03 09:02:09 +0200

    Página potencia terminada.

--- app.py
+++ app.py
@@ -6,4 +6,14 @@ app = Flask (__name__)
 def inicio():
     return render_template("inicio.html")
 
+@app.route('/potencia/<int:base>/<int:exponente>',methods=["GET","POST"])
+def potencia(base,exponente):
+    if exponente > 0:
+        resultado = base**exponente
+    elif exponente == 0:
+        resultado = 1
+    elif exponente < 0:
+        resultado = 1/(base**abs(exponente))
+    return render_template("potencia.html",ba=base,ex=exponente,res=resultado)
+
 app.run(debug=True)
